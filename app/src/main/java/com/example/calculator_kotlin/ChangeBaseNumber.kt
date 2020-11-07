95d255ed39613e472595682e388cc4a944df9164
Shota Ishii <shotaishii@ShotanoMacBook-Pro.local>
2020-11-07 22:14:56 +0900

    3rd commit

--- app/src/main/java/com/example/calculator_kotlin/ChangeBaseNumber.kt
+++ app/src/main/java/com/example/calculator_kotlin/ChangeBaseNumber.kt
@@ -2,78 +2,38 @@ package com.example.calculator_kotlin
 
 class ChangeBaseNumber{
     fun binnary(num: String): String{
-        val array = num.split("\\.".toRegex())
-        val integer = array[0].toString()
-        val decimals = "0." + array[1].toString()
-        var resultString = ""
-        var number = integer.toInt()
-        //整数部計算
-        var base = 1
-        var result = 0
-        while (number > 0){
-            result += number % 2 * base
-            number /= 2
-            base *= 10
+        if(num.indexOf(".") < 1){
+            val result = changeInteger(num,2)
+            return result.toString()
+        }else{
+            val array = num.split("\\.".toRegex())
+            val integer = array[0].toString()
+            val decimals = "0." + array[1].toString()
+            val result = changeInteger(integer,2)
+            val decimalResult = changeDecimal(decimals,2)
+
+            return (result + decimalResult).toString()
         }
-        //少数部計算
-        var decimalNumber = decimals.toDouble()
-        var decimalBase = 0.1
-        var decimalResult = 0.0
-        var decimal = 0.5
-        while (decimalNumber > 0.0){
-            if(decimalNumber / decimal == 1.0){
-                println("aaaa")
-                decimalResult += decimalNumber / decimal * decimalBase
-                println(decimalResult)
-                println("while decimalResult: " + decimalResult)
-                println("while decimalNumber: " + decimalNumber)
-            }
-            decimalNumber %= decimal
-            decimal /= 2
-            decimalBase /= 10
-        }
-        println(decimalResult)
-        resultString = (result + decimalResult).toString()
-        return resultString
     }
 
 
     fun octal(num: String): String{
-        val array = num.split("\\.".toRegex())
-        val integer = array[0].toString()
-        val decimals = "0." + array[1].toString()
-        var resultString = ""
-        var number = integer.toInt()
-        //整数部計算
-        var base = 1
-        var result = 0
-        while (number > 0){
-            result += number % 8 * base
-            number /= 8
-            base *= 10
+        if(num.indexOf(".") < 1){
+            val result = changeInteger(num,8)
+            return result.toString()
+        }else{
+            val array = num.split("\\.".toRegex())
+            val integer = array[0].toString()
+            val decimals = "0." + array[1].toString()
+            val result = changeInteger(integer,8)
+            val decimalResult = changeDecimal(decimals,8)
+
+            return (result + decimalResult).toString()
         }
-        //少数部計算
-        var decimalNumber = decimals.toDouble()
-        var decimalBase = 0.1
-        var decimalResult = 0.0
-        var decimal = 1/8
-        while (decimalNumber > 0.0){
-            if(decimalNumber / decimal == 1.0){
-                println("aaaa")
-                decimalResult += decimalNumber / decimal * decimalBase
-                println(decimalResult)
-                println("while decimalResult: " + decimalResult)
-                println("while decimalNumber: " + decimalNumber)
-            }
-            decimalNumber %= decimal
-            decimal /= 8
-            decimalBase /= 10
-        }
-        println(decimalResult)
-        resultString = (result + decimalResult).toString()
-        return resultString
     }
     fun hexadecimal(num: String): String{
+        //小数点がない場合の処理
+        //10以上時の処理
         val array = num.split("\\.".toRegex())
         val integer = array[0].toString()
         val decimals = "0." + array[1].toString()
@@ -107,5 +67,40 @@ class ChangeBaseNumber{
         println(decimalResult)
         resultString = (result + decimalResult).toString()
         return resultString
+    }
+
+
+    fun changeInteger(num: String,baseInt: Int): Int{
+        var number = num.toInt()
+        var base = 1
+        var result = 0
+        val baseNumber = baseInt
+        while (number > 0){
+            result += number % baseNumber * base
+            number /= baseNumber
+            base *= 10
+        }
+
+        return result
+    }
+    fun changeDecimal(num: String,baseDecimal:Int): Double{
+        var decimalNumber = num.toDouble()
+        var decimalBase = 0.1
+        var decimalResult = 0.0
+        var decimal = 1.0/baseDecimal
+        while (decimalNumber > 0.0){
+            if(decimalNumber - decimal > 0){
+                println("aaaa")
+                println(decimalNumber)
+                decimalResult += decimalNumber / decimal * decimalBase
+                println(decimalResult)
+                println("while decimalResult: " + decimalResult)
+                println("while decimalNumber: " + decimalNumber)
+            }
+            decimalNumber %= decimal
+            decimal /= baseDecimal
+            decimalBase /= 10
+        }
+        return decimalResult
     }
 }
